82a1fddcaa299181a95fcc60a79d2d44fa7e22dd
Andre Luchessi <andre.luchessi@outlook.com>
2022-08-09 22:39:37 -0400

    bar and scatter plot

--- barplot_admixture.R
+++ barplot_admixture.R
@@ -9,3 +9,94 @@ head(data)
 mm<- t(as.matrix(data[,c(1:5)]))
 barplot(mm,col=rainbow(5),xlab="Individual #", ylab="Ancestry",border=NA)
 
+
+
+library(ggplot2)
+library(hrbrthemes)
+library(ggpubr)
+
+data_cor<- read.delim("data_cor_r.txt", sep = "", header = T)
+head(data_cor)
+
+
+
+
+
+df<- data_cor[,c(1,2)]
+colnames(df)<- c("x","y")
+head(df)
+
+p1 <- ggplot(df, aes(x=x, y=y)) + 
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE)+
+  stat_cor(label.y = 0.8)+
+  labs(x = "EUR", y="EUR_SCORGE")+
+  xlim(0, 1)+
+  ylim(0, 1)+
+  theme_ipsum()
+p1
+
+
+df<- data_cor[,c(3,4)]
+colnames(df)<- c("x","y")
+head(df)
+
+p2 <- ggplot(df, aes(x=x, y=y)) + 
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE)+
+  stat_cor(label.y = 0.8)+
+  labs(x = "AFR", y="AFR_SCORGE")+
+  xlim(0, 1)+
+  ylim(0, 1)+
+  theme_ipsum()
+p2
+
+
+df<- data_cor[,c(5,6)]
+colnames(df)<- c("x","y")
+head(df)
+
+p3 <- ggplot(df, aes(x=x, y=y)) + 
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE)+
+  stat_cor(label.y = 0.8)+
+  labs(x = "AES", y="AES_SCORGE")+
+  xlim(0, 1)+
+  ylim(0, 1)+
+  theme_ipsum()
+p3
+
+
+df<- data_cor[,c(7,8)]
+colnames(df)<- c("x","y")
+head(df)
+
+p4 <- ggplot(df, aes(x=x, y=y)) + 
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE)+
+  stat_cor(label.y = 0.8)+
+  labs(x = "NAM", y="AMR_SCORGE")+
+  xlim(0, 1)+
+  ylim(0, 1)+
+  theme_ipsum()
+p4
+
+
+df<- data_cor[,c(9,10)]
+colnames(df)<- c("x","y")
+head(df)
+
+p5 <- ggplot(df, aes(x=x, y=y)) + 
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="#69b3a2", se=TRUE)+
+  stat_cor(label.y = 0.08)+
+  labs(x = "SAS", y="SAS_SCORGE")+
+  xlim(0, 0.3)+
+  ylim(0, 0.1)+
+  theme_ipsum()
+p5
+
+
+ggarrange(p1, p2, p3, p4, p5 + rremove("x.text"), 
+          labels = c("A", "B", "C", "D" ,"E"),
+          ncol = 2, nrow = 3)
